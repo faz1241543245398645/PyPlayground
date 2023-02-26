d2814e579731f462e8615ae720658728e5b5866e
faz1241543245398645 <111303147+faz1241543245398645@users.noreply.github.com>
2023-02-26 14:06:04 +0800

    Update color.py

--- color.py
+++ color.py
@@ -2,3 +2,6 @@ import colorama
 from colorama import Fore
 
 print(Fore.RED + 'This text is red in color')
+
+
+#command prompt does not support ANSI escape codes, which are used in the code above to apply text styles , colors and backgrounds
